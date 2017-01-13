7a6538848d9b99a0af457676f859ef8de26d0340
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-13 15:49:04 -0300

    Update

--- controllers/invitacion.js
+++ controllers/invitacion.js
@@ -3,6 +3,8 @@ const Hash = require('hashids');
 const Model = require('../db/model');
 const config = require('../config/main');
 
+var hashId = new Hash(config.secret);
+
 module.exports = function(req, id) {
     console.log('ID: ' + id);
     console.log('ID Decoded:' + hashId.decode(id));
