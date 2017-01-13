7a572bd6a5656dfd88ea8915631e6581b51b7b2a
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-13 14:40:58 -0300

    Update

--- controllers/invitacion.js
+++ controllers/invitacion.js
@@ -1,5 +1,11 @@
+const express = require('express');
+const config = require('./config/main');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
 module.exports = function(id) {
-    app.set('view engine', 'pug');
     console.log('ID: ' + id);
     res.render(
 		'confirmaInvitacion',
