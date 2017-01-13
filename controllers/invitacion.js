9d0e308ab2aecd0e2493a9842abe7a4d326ad41c
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-13 17:11:51 -0300

    Update

--- controllers/invitacion.js
+++ controllers/invitacion.js
@@ -1,23 +1,30 @@
-//const express = require('express');
 const Hash = require('hashids');
 const Model = require('../db/model');
-const config = require('../config/main');
 
 var hashId = new Hash(config.secret);
 
 module.exports = function (req, res, id) {
-    id = String(hashId.decode(id)).slice(9);
+    var idDecoded = String(hashId.decode(id)).slice(9),
+        mensaje,
+        icono;
 
     new Model.Invitacion({
-        pInvitacion: id
+        pInvitacion: idDecoded
     }).save({
         bRecibido: '1'
     }, {
             patch: true
         }).then(function (data) {
+            if (data === null) {
+                icono = 'error_outline';
+                mensaje = 'La invitación no es válida. Vuelva a solicitar una.';
+            } else {
+                icono = 'done';
+                mensaje = 'Usted ha confirmado la invitación.';
+            }
             res.render(
 		        'confirmaInvitacion',
-		        { idInvitacion: id }
+		        { idInvitacion: id, mInvitacion: mensaje, mIcono: icono }
 	        );
         });
 };
