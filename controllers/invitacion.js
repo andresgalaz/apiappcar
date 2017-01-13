e735dd5f8b9f355149019f2102e1efbf5209c8fb
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-13 17:42:04 -0300

    Update

--- controllers/invitacion.js
+++ controllers/invitacion.js
@@ -30,6 +30,7 @@ module.exports = function (req, res, id) {
     new Model.Invitacion({
         pInvitacion: idDecoded
     }).fetch().then(function (data) {
+        console.log('data.bRecibido:', data.bRecibido);
         if (data.bRecibido === '1') {
             estado = 'YA LA ACEPTASTE';
         } else {
