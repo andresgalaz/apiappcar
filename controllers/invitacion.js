87da20dbb4bed447b31ccb786edeec2caa0ce501
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-16 11:20:02 -0300

    Actualizado Mon Jan 16 2017 11:20:02

--- controllers/invitacion.js
+++ controllers/invitacion.js
@@ -28,8 +28,8 @@ module.exports = function (req, res, id) {
                     }
                 });
         }
-        
     }).then(function () {
+        console.log('ESTADO:', estado);
         res.render(
             'confirmaInvitacion',
             { idInvitacion: id, estadoInvitacion: estado }
