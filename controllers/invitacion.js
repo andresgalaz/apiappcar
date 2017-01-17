d0a0e721163d50fd8c784ae009a3c3ef04acccfe
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-17 10:38:47 -0300

    Actualizado Tue Jan 17 2017 10:38:47

--- controllers/invitacion.js
+++ controllers/invitacion.js
@@ -8,33 +8,30 @@ var hashId = new Hash(config.secret),
 module.exports = function (req, res, id) {
     var idDecoded = String(hashId.decode(id)).slice(9);
 
-    new Model.Invitacion({
-        pInvitacion: idDecoded
-    }).fetch().then(function (data) {
-        try {
-            if (data.attributes.bRecibido === '1') {
-                estado = 'aceptado';
-            } else {
-                this.save({
-                    bRecibido: '1'
-                }, {
-                        patch: true
-                    }).then(function (data) {
-                        if (data === null) {
-                            estado = 'error';
-                        } else {
-                            estado = 'exito';
-                        }
-                    });
+    new Model.Invitacion({ pInvitacion: idDecoded })
+        .fetch()
+        .then(function (data) {
+            try {
+                if (data.attributes.bRecibido === '1') {
+                    estado = 'aceptado';
+                } else {
+                    this.save({ bRecibido: '1' }, { patch: true })
+                        .then(function (data) {
+                            if (data === null) {
+                                estado = 'error';
+                            } else {
+                                estado = 'exito';
+                            }
+                        });
+                }
+            } catch (err) {
+                estado = 'error';
+                console.log(err);
             }
-        } catch (err) {
-            estado = 'error';
-            console.log(err);
-        }
-    }).then(function () {
-        res.render(
-            'confirmaInvitacion',
-            { idInvitacion: id, estadoInvitacion: estado }
-        );
-    });
+        }).then(function () {
+            res.render(
+                'confirmaInvitacion',
+                { idInvitacion: id, estadoInvitacion: estado }
+            );
+        });
 };
