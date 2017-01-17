2309d6e5805e2d1494775ce98f5fc5770ce8cda6
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-17 16:05:22 -0300

    Actualizado Tue Jan 17 2017 16:05:22

--- servicio/wsInvitacion.js
+++ servicio/wsInvitacion.js
@@ -64,6 +64,7 @@ module.exports = function (req, res) {
 						dataIns.vehiculos().attach(arrVeh);
 
 						// Template
+						app.locals.basedir = './views';
 						const cEmailBody = pug.compileFile('views/emailInvitacion.pug');
 
 						// Envía correo al usuario invitado
