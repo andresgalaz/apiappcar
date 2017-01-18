819a67e8df5b2efa4731cf5bca65caba951df257
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-18 13:05:30 -0300

    Update

--- servicio/wsRegistro.js
+++ servicio/wsRegistro.js
@@ -90,7 +90,7 @@ module.exports = function (req, res) {
 						console.log('HASH ID:', hashId);
 
 						const cEmailBody = pug.compileFile('views/emailRegistro.pug');
-						var idRegistro = hashId.encode(req.body.email);
+						var idRegistro = hashId.encode('test');
 
 						console.log('EMAIL:', req.body.email);
 						console.log('ID REGISTRO:', idRegistro);
