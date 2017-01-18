a9ff775f75df9b2ab42408fff6f40a7664f855f4
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-18 16:47:58 -0300

    Actualizado Wed Jan 18 2017 16:47:58

--- controllers/registro.js
+++ controllers/registro.js
@@ -2,7 +2,7 @@ const Hash = require('hashids');
 const Model = require('../db/model');
 const config = require('../config/main');
 
-var hashId = new Hash(config.secret);    
+var hashId = new Hash(config.secret);
 
 module.exports = function (req, res, id) {
     var idDecoded = String(hashId.decode(id)).slice(9);
@@ -22,27 +22,15 @@ module.exports = function (req, res, id) {
                             } else {
                                 estado = 'exito';
                             }
+                            res.render(
+                                'confirmaRegistro',
+                                { idRegistro: id, estadoRegistro: estado }
+                            );
                         });
                 }
             } catch (err) {
                 console.log(err);
                 estado = 'error';
             }
-        })
-        */
-        .save({ bConfirmado: '1' }, { patch: true })
-        .then(function (data) {
-            if (data === null) {
-                estado = 'error';
-            } else {
-                estado = 'exito';
-            }          
-
-            console.log("ESTADO:", estado);
-
-            res.render(
-                'confirmaRegistro',
-                { idRegistro: id, estadoRegistro: estado }
-            );
         });
 };
