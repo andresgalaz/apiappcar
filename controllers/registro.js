d9a08ad6b4073cd0f0207bf2531f1846d37759b0
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-18 16:13:47 -0300

    Actualizado Wed Jan 18 2017 16:13:47

--- controllers/registro.js
+++ controllers/registro.js
@@ -30,6 +30,7 @@ module.exports = function (req, res, id) {
             }
         })
         .then(function () {
+            console.log("ESTADO:", estado);
             res.render(
                 'confirmaRegistro',
                 { idRegistro: id, estadoRegistro: estado }
