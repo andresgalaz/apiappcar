4cce56cc4f5cb8dee34c2aef9f3b400591c90b9e
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-19 17:51:49 -0300

    Actualizado Thu Jan 19 2017 17:51:48

--- servicio/wsRecuperaPassword.js
+++ servicio/wsRecuperaPassword.js
@@ -2,19 +2,19 @@
 const Model = require('../db/model');
 const moment = require("moment");
 
-module.exports = function(req,res){
+module.exports = function (req, res) {
 
 	// Registra nuevos usuarios o usuarios existentes en dispositivos nuevos
 	console.log('---------', moment().format("YYYY-MM-DD HH:mm:ss"), '--------');
 	console.log(req.body);
-	if(!req.body.email ) {
+	if (!req.body.email) {
 		return res.status(400).json({ success: false, code: 1210, message: 'Falta email.' });
 	}
-	new Model.Usuario({cEmail: req.body.email}).fetch().then(function(data){
-		if (data !== null ) {
+	new Model.Usuario({ cEmail: req.body.email }).fetch().then(function (data) {
+		if (data !== null) {
 			// Almacena nueva contraseña y envía email
-			
-			
+			var nuevoPassword = parseInt(Math.random()*10e6);
+
 			res.status(201).json({ success: true, message: 'Correo electrónico enviado' });
 		} else {
 			// No existe y se crea el usuario
