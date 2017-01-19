644d978f8ee6950e3605d7c83c7cb95fbc45bfd4
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-19 14:34:54 -0300

    Actualizado Thu Jan 19 2017 14:34:54

--- servicio/wsRegistro.js
+++ servicio/wsRegistro.js
@@ -81,7 +81,7 @@ module.exports = function (req, res) {
 
 			if (pUsuario) {
 				newUser
-					//.fetch({ pUsuario: pUsuario })
+					.fetch({ pUsuario: pUsuario })
 					.save({ patch: true })
 					.then(function (data) {
 						user = data.toJSON();
