9d861d044ceda51bbab49d2359f3c505ace6f2cf
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-19 11:23:00 -0300

    Actualizado Thu Jan 19 2017 11:23:00

--- controllers/invitacion.js
+++ controllers/invitacion.js
@@ -2,36 +2,41 @@ const Hash = require('hashids');
 const Model = require('../db/model');
 const config = require('../config/main');
 
-var hashId = new Hash(config.secret),
-    estado = null;
+var hashId = new Hash(config.secret);
 
 module.exports = function (req, res, id, estado) {
-    var idDecoded = String(hashId.decode(id)).slice(9);
+    var idDecoded = String(hashId.decode(id)).slice(9),
+        newInvitacion = new Model.Invitacion({ pInvitacion: idDecoded }),
+        template = function (estado) {
+			res.render(
+				'confirmaRegistro',
+				{ idRegistro: id, estadoRegistro: estado }
+			);
+		};
 
-    new Model.Invitacion({ pInvitacion: idDecoded })
+    newInvitacion
         .fetch()
         .then(function (data) {
             try {
                 if (data.attributes.bRecibido === '1') {
-                    estado = 'aceptado';
+                    //estado = 'aceptado';
+                    template('aceptado');
                 } else {
                     this.save({ bRecibido: '1' }, { patch: true })
                         .then(function (data) {
                             if (data === null) {
-                                estado = 'error';
+                                //estado = 'error';
+                                template('error');
                             } else {
-                                estado = 'exito';
+                                //estado = 'exito';
+                                template('exito');
                             }
                         });
                 }
             } catch (err) {
-                estado = 'error';
+                //estado = 'error';
                 console.log(err);
+                template('error');
             }
-        }).then(function () {
-            res.render(
-                'confirmaInvitacion',
-                { idInvitacion: id, estadoInvitacion: estado }
-            );
         });
 };
