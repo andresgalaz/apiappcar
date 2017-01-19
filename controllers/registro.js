d1db3d14b74855522cfbb6bcaaf2a19fe72c6d4c
rodrigosobrero <hello@rodrigosobrero.com>
2017-01-19 10:14:15 -0300

    Actualizado Thu Jan 19 2017 10:14:14

--- controllers/registro.js
+++ controllers/registro.js
@@ -2,8 +2,8 @@ const Hash = require('hashids');
 const Model = require('../db/model');
 const config = require('../config/main');
 
-var hashId = new Hash(config.secret);
-estado = null;
+var hashId = new Hash(config.secret),
+    estado = null;
 
 module.exports = function (req, res, id) {
     var idDecoded = String(hashId.decode(id)).slice(9),
