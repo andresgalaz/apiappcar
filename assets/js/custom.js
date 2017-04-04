3bc16856b43c26dd96d2610c1db7e41bcd36765e
Rodrigo Sobrero <hola@rodrigosobrero.com>
2017-04-04 17:43:34 -0300

    Ventana confirmación de aceptación de legales

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#modalLegales').modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+
+    $('#accordion').find('a').first().on('click', function () {
+        $('.fa.fa-angle-right').first().toggleClass('fa-open');
+        politicasPrivacidad = true;
+        chequeaEstado();
+    });
+
+    $('#accordion').find('a').last().on('click', function () {
+        $('.fa.fa-angle-right').last().toggleClass('fa-open');
+        terminosCondiciones = true;
+        chequeaEstado();
+    });
+
+    function chequeaEstado() {
+        if (terminosCondiciones && politicasPrivacidad) {
+            $('.modal-footer button').removeAttr('disabled');
+        }
+    }
+});
