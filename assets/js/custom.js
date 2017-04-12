c3dbb463125e067cb6453e8cd30a900a409a9312
Rodrigo Sobrero <rodrigosobrero@MacBook-Pro-de-Rodrigo.local>
2017-04-12 12:19:12 -0300

    Actualizado Wed Apr 12 2017 12:19:12

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,20 +8,18 @@ $(document).ready(function () {
     });
 
     $('#accordion').find('a').first().on('click', function () {
-        $('.fa.fa-angle-right').first().toggleClass('fa-open');
         politicasPrivacidad = true;
         chequeaEstado();
     });
 
     $('#accordion').find('a').last().on('click', function () {
-        $('.fa.fa-angle-right').last().toggleClass('fa-open');
         terminosCondiciones = true;
         chequeaEstado();
     });
 
     function chequeaEstado() {
         if (terminosCondiciones && politicasPrivacidad) {
-            $('.modal-footer button').removeAttr('disabled');
+            $('.modal-footer input[type=submit]').removeAttr('disabled');
         }
     }
 });
