38ae18e1daa5bd611e145c9933b708c9bd07ea82
Rodrigo Sobrero <rodrigosobrero@MacBook-Pro-de-Rodrigo.local>
2017-04-12 14:13:14 -0300

    Actualizado Wed Apr 12 2017 14:13:14

--- controllers/confirmaRegistro.js
+++ controllers/confirmaRegistro.js
@@ -3,7 +3,7 @@ const Model = require('../db/model');
 const config = require('../config/main');
 
 module.exports = function (req, res) {
-    var confirmaRegistro = function () {
+    var confirmaRegistro = function (req, res) {
         console.log(req.body);
     };
 };
