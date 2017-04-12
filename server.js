90475191a01c2aa1224429a02e11eb9363b0fce3
Rodrigo Sobrero <rodrigosobrero@MacBook-Pro-de-Rodrigo.local>
2017-04-12 14:26:26 -0300

    Actualizado Wed Apr 12 2017 14:26:26

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/registro', function (req, res) {
 });
 
 // Nuevo
-app.get('/registro-confirma', function (req, res) {
+app.get('/registro/confirma', function (req, res) {
   require('./controllers/confirmaRegistro.js')(req, res);
 });
 
