a763ee0008468dec1838bc82ed043c93b941d024
Rodrigo Sobrero <rodrigosobrero@MacBook-Pro-de-Rodrigo.local>
2017-04-12 14:45:17 -0300

    Actualizado Wed Apr 12 2017 14:45:17

--- controllers/confirmaRegistro.js
+++ controllers/confirmaRegistro.js
@@ -5,6 +5,13 @@ const config = require('../config/main');
 module.exports = function (req, res) {
     var confirmaRegistro = function (req, res) {
         console.log(req.body);
+
+
+
+        res.render(
+			'confirmaRegistro',
+			{ idRegistro: '1234', estadoRegistro: 'Test' }
+		);
     };
 
     confirmaRegistro(req, res);
