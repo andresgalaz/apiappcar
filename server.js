62eb032355c21ad380ee099b220fac2a6817835a
Rodrigo Sobrero <rodrigosobrero@MacBook-Pro-de-Rodrigo.local>
2017-04-12 16:17:13 -0300

    Actualizado Wed Apr 12 2017 16:17:13

--- server.js
+++ server.js
@@ -17,26 +17,26 @@ app.set('view engine', 'pug');
 // Define el puerto de acuerdo al ambiente
 var port = null;
 if (config.ambiente == 'DESA')
-	port = 2080;
+  port = 2080;
 else if (config.ambiente == 'PROD')
-	port = 8090;
+  port = 8090;
 
-var allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
-    }
-    else {
-      next();
-    }
+var allowCrossDomain = function (req, res, next) {
+  if ('OPTIONS' == req.method) {
+    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  }
+  else {
+    next();
+  }
 };
 
 // Use body-parser to get POST requests for API use
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -46,34 +46,29 @@ app.use(morgan('dev'));
 
 // Home route. We'll end up changing this to our main front end index later.
 app.get('/', function (req, res) {
-	res.send('Página en construcción.');
+  res.send('Página en construcción.');
 });
 
 app.get('/bitacora', function (req, res) {
-	res.sendfile('server_' + config.ambiente + '.log');
+  res.sendfile('server_' + config.ambiente + '.log');
 });
 
 // Página de confirmación de invitación
 app.get('/invitacion', function (req, res) {
-	var id = req.query.id;
-	require('./controllers/invitacion.js')(req, res, id);
+  var id = req.query.id;
+  require('./controllers/invitacion.js')(req, res, id);
 });
 
 // Página de confirmación de registro
 app.get('/registro', function (req, res) {
-	var id = req.query.id;
-	require('./controllers/registro.js')(req, res, id);
+  var id = req.query.id;
+  require('./controllers/registro.js')(req, res, id);
 });
 
 // Nuevo
 app.get('/registro/confirma', function (req, res, status) {
   var status = req.query.status;
-
-  if (status == '1') {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(400);
-  }
+  require('./controllers/confirma.js')(req, res, status);
 });
 
 require('./app/routes')(app);
