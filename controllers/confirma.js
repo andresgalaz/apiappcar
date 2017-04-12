70d27de2e35dc8cf65c3f0d8e36ee1856d0b35bd
Rodrigo Sobrero <rodrigosobrero@MacBook-Pro-de-Rodrigo.local>
2017-04-12 16:18:55 -0300

    Actualizado Wed Apr 12 2017 16:18:55

--- controllers/confirma.js
+++ controllers/confirma.js
@@ -2,11 +2,27 @@ const Hash = require('hashids');
 const Model = require('../db/model');
 const config = require('../config/main');
 
-module.exports = function (req, res) {
-    var confirmaRegistro = function (req, res) {
-        console.log(req.body);
-        return req.body;
-    };
+module.exports = function (req, res, status) {
 
-    confirmaRegistro(req, res);
+    if (status == '1') {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(400);
+
+        /* Reenvía email de confirmación */
+        email.server.send({
+            from: 'SnapCar <no-responder@snapcar.com.ar>',
+            to: req.body.email,
+            subject: 'Confirme su registro',
+            attachment: [{
+                data: cEmailBody({
+                    nombreUsuario: req.body.nombre.split(' ')[0],
+                    idRegistro: idRegistro,
+                    baseUrl: req.protocol + '://' + req.headers.host
+                }),
+                alternative: true
+            }]
+        }, function (err, message) { console.log(err || message); });
+    }
+
 };
