0b049d79549c60cf31ba002f6b9c46e805434c4f
Rodrigo Sobrero <rodrigosobrero@MacBook-Pro-de-Rodrigo.local>
2017-04-24 18:28:12 -0300

    Update

--- servicio/wsLogin.js
+++ servicio/wsLogin.js
@@ -58,7 +58,7 @@ module.exports = function (req, res) {
 						}
 					} else if (req.body.facebook) {
 						FB.api(
-							'graph.facebook.com/debug_token?input_token=' + req.body.facebook,
+							'/debug_token?input_token=' + req.body.facebook,
 							function (response) {
 								if (response && !response.error) {
 									console.log('FACEBOOK', response);
