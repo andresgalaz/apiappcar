d1dcb8f0b39e4c71abc4b52f7f5eb45f74e03da4
Andres Galaz <andres.galaz@gmail.com>
2017-06-30 13:04:33 -0300

    Instalacion

--- servicio/wsInstalacion.js
+++ servicio/wsInstalacion.js
@@ -5,22 +5,16 @@ module.exports = function(req, res) {
     // Registra el virloc a la patente ingresada
     console.log('---------', moment().format("YYYY-MM-DD HH:mm:ss"), '--------');
     console.log(req.body);
-    if (!req.body.opcion) {
-        return res.status(400).json({ success: false, code: 3510, message: 'Falta parámetro `opción`.' });
+    if (!req.body.accion) {
+        return res.status(400).json({ success: false, code: 3510, message: 'Falta parámetro accion.' });
     }
-    if (!req.body.estado) {
-        return res.status(400).json({ success: false, code: 3512, message: 'Falta estado.' });
-    }
-    if (!req.body.id) {
-        return res.status(400).json({ success: false, code: 3514, message: 'Falta ID. de dispositivo.' });
-    }
-    if (req.body.opcion.length > 20) {
+    if (req.body.accion.length > 20) {
         return res.status(400).json({ success: false, code: 3516, message: 'Opción no válida.' });
     }
-    if (req.body.estado.length > 20) {
+    if (req.body.estado && req.body.estado.length > 20) {
         return res.status(400).json({ success: false, code: 3518, message: 'Estado no válido.' });
     }
-    if (req.body.id.length > 50) {
+    if (req.body.id && req.body.id.length > 50) {
         return res.status(400).json({ success: false, code: 3520, message: 'ID. de dispositivo no válido.' });
     }
     if (req.body.patente && req.body.patente.length > 20) {
@@ -30,14 +24,19 @@ module.exports = function(req, res) {
         return res.status(400).json({ success: false, code: 3524, message: 'Nº instalación no válido.' });
     }
 
-    var arrPrm = [req.user.pUsuario, req.body.opcion, req.body.estado, req.body.id, req.body.patente, req.body.num_instalacion];
+    var arrPrm = [req.user.pUsuario, req.body.accion, req.body.id, req.body.patente, req.body.estado, req.body.num_instalacion];
     console.log(arrPrm);
     db.scoreDB.knex.raw("call prInstalacion(?,?,?,?,?,?)", arrPrm)
         .then(function(data) {
             try {
                 var arr = data[0][0][0];
-                if (arr.nCodigo == 0)
-                    return res.status(201).json({ success: true });
+                console.log(arr.LOG);
+                if (arr.nCodigo == 0) {
+                    var oResp = { success: true };
+                    if (typeof arr.cPatenteActual != undefined) oResp.patenteActual = arr.cPatenteActual;
+                    if (typeof arr.cIdDispositivo != undefined) oResp.id = arr.cIdDispositivo;
+                    return res.status(201).json(oResp);
+                }
                 if (arr.nCodigo > 0)
                     return res.status(400).json({ success: false, code: arr.nCodigo, message: arr.cMensaje });
                 return res.status(400).json({ success: false, code: 3526, message: 'Error inesperado al recibir resultado' });
