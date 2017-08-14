08bf90d3fcb09b3da612a6280275474a7805583c
SnapCar <admin@snapcar.com.ar>
2017-08-14 18:47:36 +0000

    log wsScoreVehiculo

--- servicio/wsScoreVehiculo.js
+++ servicio/wsScoreVehiculo.js
@@ -19,7 +19,6 @@ module.exports = function(req, res) {
             return res.status(400).json({ success: false, code: 2010, message: "Periodo debe se numérico." });
         if (nPeriodo > 0)
             return res.status(400).json({ success: false, code: 2012, message: "Periodo debe ser negativo" });
-//		nPeriodo++;
     } else {
         if (!req.body.fechaInicio || !req.body.fechaFin)
             return res.status(400).json({ success: false, code: 2016, message: "Se debe indicar periodo o rango de fechas." });
@@ -52,8 +51,7 @@ module.exports = function(req, res) {
             return res.status(400).json({ success: false, code: 2012, message: "Id. Usuario debe ser mayor que cero" });
     }
 
-console.log("call prScoreVehiculoRangoFecha(?,?,?,?,?,?)", [req.user.pUsuario, nPeriodo, cFecIni, cFecFin, nIdVehiculo, nIdConductor]);
-    db.scoreDB.knex.raw("call prScoreVehiculoRangoFecha(?,?,?,?,?,?)", [req.user.pUsuario, nPeriodo, cFecIni, cFecFin, nIdVehiculo, nIdConductor]).then(function(data) {
+    db.scoreDB.knex.raw("call prScoreVehiculoRangoFecha(?,?,?,?,?,?,?)", [req.user.pUsuario, nPeriodo, cFecIni, cFecFin, nIdVehiculo, nIdConductor, req.body.noViajes=='true']).then(function(data) {
         if (data === null) {
             return res.status(400).json({ success: false, code: 2024, message: "Error al ejecutar consulta Score de Vehiculos" });
         }
