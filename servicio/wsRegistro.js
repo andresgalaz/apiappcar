573a379111a69a26c4c41f7505bf88618c7e45e3
Andres Galaz <andres.galaz@gmail.com>
2017-11-21 16:30:01 -0300

    Se crea el registro en Hubsopt al registrar

--- servicio/wsRegistro.js
+++ servicio/wsRegistro.js
@@ -1,11 +1,44 @@
 const Model = require('../db/model');
 const config = require('../config/main');
 const email = require('../config/emailServer');
-const moment = require("moment");
+const moment = require('moment');
 const pug = require('pug');
 const Hash = require('hashids');
+const Hubspot = require('hubspot');
 
 module.exports = function(req, res) {
+
+	function enviaMailRegistro( pUsuario, cEmail, cNombre ){
+        const cEmailBody = pug.compileFile('views/emailRegistro.pug');
+        var hashId = new Hash(config.secret);        
+		var idRegistro = hashId.encode(1e11 + pUsuario);
+		var toMail = [req.body.email];
+		var linkUrl = 'https://desa.snapcar.com.ar/wappTest'
+		var baseUrl = 'https://test.appcar.com.ar/'
+		if (process.env.WSAPI_AMBIENTE == 'PROD') {
+			linkUrl = 'https://app.snapcar.com.ar/wappCar'
+			baseUrl = 'https://api.appcar.com.ar/'
+		}
+		linkUrl += '/do/cli/login/registro.vm';
+		email.server.send({
+			from: 'SnapCar Integrity <no-responder@snapcar.com.ar>',
+			to: toMail,
+			subject: 'Confirme su registro',
+			attachment: [{
+				data: cEmailBody({
+					nombreUsuario: cNombre.split(/ +/)[0],
+					idRegistro: idRegistro,
+					emailRegistro: cEmail,
+					baseUrl: baseUrl,
+					linkUrl: linkUrl
+				}),
+				alternative: true
+			}]
+		}, function(err, message) {
+			if(err) console.error(err);
+		});
+	}
+
     const Util = require('../util');
 
     // Registra nuevos usuarios o usuarios existentes en dispositivos nuevos
@@ -63,11 +96,14 @@ module.exports = function(req, res) {
                     user = data.toJSON();
                     user.success = false;
                     user.message = 'Se le ha enviado un mail. Confirme el mail para ingresar';
+					try {
+						enviaMailRegistro( user.pUsuario, req.body.email, req.body.nombre );
+					} catch(e){
+						console.error( e );
+					}
                     return res.status(400).json(user);
                 });
             } else {
-                var hashId = new Hash(config.secret);
-
                 new Model.Usuario({
                     cEmail: req.body.email,
                     cPassword: req.body.password,
@@ -83,38 +119,47 @@ module.exports = function(req, res) {
                         var usrOut = Model.UsuarioVeh.salida(user);
 						usrOut.success = true;
 						usrOut.message ='Se le ha enviado un mail. Confirme el mail para ingresar';
+						try {
+							enviaMailRegistro( user.pUsuario, req.body.email, req.body.nombre );
+						} catch(e){
+							console.error( e );
+						}
 						return res.status(201).json(usrOut);
                     });
-
-                    const cEmailBody = pug.compileFile('views/emailRegistro.pug');
-                    var idRegistro = hashId.encode(1e11 + user.pUsuario);
-					var toMail = [req.body.email];
-                    var linkUrl = 'https://desa.snapcar.com.ar/wappTest'
-                    var baseUrl = 'https://test.appcar.com.ar/'
-					if (process.env.WSAPI_AMBIENTE == 'PROD') {
-                    	linkUrl = 'https://app.snapcar.com.ar/wappCar'
-                    	baseUrl = 'https://api.appcar.com.ar/'
-					}
-					linkUrl += '/do/cli/login/registro.vm';
-                    email.server.send({
-                        from: 'SnapCar Integrity <no-responder@snapcar.com.ar>',
-                        to: toMail,
-                        subject: 'Confirme su registro',
-                        attachment: [{
-                            data: cEmailBody({
-                                nombreUsuario: req.body.nombre.split(' ')[0],
-                                idRegistro: idRegistro,
-                                emailRegistro: req.body.email,
-                                baseUrl: baseUrl,
-                                linkUrl: linkUrl
-                            }),
-                            alternative: true
-                        }]
-                    }, function(err, message) {
-						if(err)
-               				console.log(err);
-                    });
+			
                 });
+                const hubspot = new Hubspot({ apiKey: '49dfd52a-a898-424f-8f38-fe2eaaab5fa6' });
+                hubspot.contacts.getByEmail( req.body.email )
+                .then( /* Si existe no hacemos nada */ )
+                .catch(data => {
+                    if( data.statusCode == 404 ){                    
+                        // Si no existe el contacto en Hubspot lo creamos
+                        hubspot.contacts.create({
+                            properties: [
+                                {
+                                    property: 'firstname',
+                                    value: req.body.nombre.split(/ +/)[0]
+                                }, {
+                                    property: 'lastname',
+                                    value: req.body.nombre.split(/ +/)[1]
+                                }, {
+                                    property: 'sexo',
+                                    value: req.body.sexo
+                                }, {
+                                    property: 'nacimiento',
+                                    value: moment(req.body.fechaNacimiento).valueOf()
+                                }, {
+                                    property: 'email',
+                                    value: req.body.email
+                                }
+                            ]
+                        })
+                        .then(/* Nada */ )
+                        .catch(err => {
+                            console.error(err);
+                        });
+                    }
+                })                
             }
         } catch (e) {
             console.log(e);
